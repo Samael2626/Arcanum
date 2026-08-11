50245483cc9ffe40ec6424a3875814c0e710c045
Samuel <samuelescobar1226@gmail.com>
2026-08-11 14:24:59 -0500

    fix(android): preparar gradle para release

--- arcanum_app/android/settings.gradle.kts
+++ arcanum_app/android/settings.gradle.kts
@@ -21,6 +21,9 @@ plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "9.0.1" apply false
     id("org.jetbrains.kotlin.android") version "2.3.20" apply false
+    // Firebase: se declara aqui, no en un buildscript{} de la raiz, que es
+    // la forma que Gradle 8+ desaconseja y que release_readiness_test veta.
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 include(":app")
